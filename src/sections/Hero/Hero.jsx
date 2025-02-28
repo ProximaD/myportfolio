dc7ac06f08eb5e748e25e4aa32fa141878ddea75
ProximaD <111272273+ProximaD@users.noreply.github.com>
2025-02-28 15:41:53 +0300

    Added Sun themeIcon

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,22 @@
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
+import themeIcon from '../../assets/sun.svg';
 
 function Hero() {
   return <section id="hero">
-    <div>
-      <img className={styles.hero} src={heroImg} alt="Profile picture of Sandra Lavenda" />
+    <div className={styles.colorModeContainer}>
+      <img 
+      className={styles.hero} 
+      src={heroImg} a
+      lt="Profile picture of Sandra Lavenda" 
+      />
+
+      <img 
+      className={styles.colorMode} 
+      src={themeIcon} 
+      alt="Color mode icon" 
+      />
+
     </div>
   </section>;
 }
