604916235e9e4f151ef75f1455d3893f81be9889
ProximaD <111272273+ProximaD@users.noreply.github.com>
2025-02-28 10:16:45 +0300

    Updated project files

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png';
+
+function Hero() {
+  return <section id="hero">
+    <div>
+      <img src="" alt="Hero Image" />
+    </div>
+  </section>;
+}
+
+export default Hero;
